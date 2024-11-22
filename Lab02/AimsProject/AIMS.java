a3248d008b1e4778ff9768c04e7c2519d3f776fa
Khải Nguyễn Thiện <nguyenthienkhai2005@gmail.com>
2024-11-22 17:37:29 +0700

    Update AIMS.java

--- Lab02/AimsProject/AIMS.java
+++ Lab02/AimsProject/AIMS.java
@@ -1,17 +1,25 @@
-package App;
+package hust.soict.cybersec.aims;
+import hust.soict.cybersec.aims.cart.Cart;
+import hust.soict.cybersec.aims.disc.DigitalVideoDisc;
 
-public class AIMS {
-	public static void main(String[] args) {
-		Cart anOrder = new Cart();
-		DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
-		anOrder.addDigitalVideoDisc(dvd1);
-		DigitalVideoDisc dvd2 =new DigitalVideoDisc("Star Wars", "Science Fiction", " George Lucas", 87, 24.95f);
-		anOrder.addDigitalVideoDisc(dvd2);
-		DigitalVideoDisc dvd3 =new DigitalVideoDisc("Aladin", "Animation", 18.99f);
-		anOrder.addDigitalVideoDisc(dvd3);
-		System.out.println("Total Cost is: ");
-		System.out.println(anOrder.totalCost());
-		anOrder.removeDigitalVideoDisc(dvd3);
-		System.out.println(anOrder.totalCost());
-	}
+public class Aims {
+ 
+    public static void main(String[] args) {
+     
+        Cart anOder = new Cart();
+        
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f); 
+        anOder.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star War", "Science Fiction", "George Lucas", 87, 25.95f); 
+        anOder.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        anOder.addDigitalVideoDisc(dvd3);
+
+        System.out.println("Total cost is: ");
+        System.out.println(anOder.totalCost());
+
+    }
+
 }
